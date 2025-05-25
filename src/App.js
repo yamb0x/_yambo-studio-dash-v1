96c08f27c2feaeaead2830504a8c00e712b2d67a
Yambo Studio <yamb0x.eth@studio.com>
2025-05-25 15:24:29 +0300

    Fix build error - move debug log to useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
-// Temporary debug - remove after fixing
-console.log('ENV CHECK:', {
-  nodeEnv: process.env.NODE_ENV,
-  hasApiKey: !!process.env.REACT_APP_FIREBASE_API_KEY,
-  apiKeyLength: process.env.REACT_APP_FIREBASE_API_KEY?.length || 0
-});
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -34,6 +27,13 @@ function AppContent() {
   useEffect(() => {
     localStorage.setItem('darkMode', isDarkMode);
     testFirebaseConnection();
+    
+    // Temporary debug - remove after fixing
+    console.log('ENV CHECK:', {
+      nodeEnv: process.env.NODE_ENV,
+      hasApiKey: !!process.env.REACT_APP_FIREBASE_API_KEY,
+      apiKeyLength: process.env.REACT_APP_FIREBASE_API_KEY?.length || 0
+    });
   }, [isDarkMode]);
 
   const toggleDarkMode = () => {
